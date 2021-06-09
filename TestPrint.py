d50a5fca9206415f434c917075438ffae14b8732
brandonnham <brandon.nham@gmail.com>
2021-06-08 18:49:22 -0600

    Grabs cnn topics

--- TestPrint.py
+++ TestPrint.py
@@ -2,4 +2,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = webdriver.Chrome();
+driver = webdriver.Chrome('C:/Users/Brand/Desktop/Software/WebDrivers/chromedriver.exe')
+driver.get('http://cnn.com')
+
+results = []
+content = driver.page_source
+soup = BeautifulSoup(content)
+
+for element in soup.findAll(attrs='cd__wrapper'):
+    name = element.find('h3')
+    if name not in results:
+        results.append(name.text)
+print(results)
